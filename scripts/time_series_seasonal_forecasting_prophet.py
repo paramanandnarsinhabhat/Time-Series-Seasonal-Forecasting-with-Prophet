8741de0cd8761b7a57f2a7310d9f8c93e52195be
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-04 11:24:39 +0530

    commit changes

--- scripts/time_series_seasonal_forecasting_prophet.py
+++ scripts/time_series_seasonal_forecasting_prophet.py
@@ -16,3 +16,25 @@ valid_data = pd.read_csv("/Users/paramanandbhat/Downloads/8._prophet/data/valid_
 
 print(train_data.shape)
 print(train_data.head())
+
+# Required Preprocessing 
+
+train_data.timestamp = pd.to_datetime(train_data['Date'],format='%Y-%m-%d')
+train_data.index = train_data.timestamp
+
+valid_data.timestamp = pd.to_datetime(valid_data['Date'],format='%Y-%m-%d')
+valid_data.index = valid_data.timestamp
+
+plt.figure(figsize=(12,8))
+
+plt.plot(train_data.index, train_data['count'], label='train_data')
+plt.plot(valid_data.index,valid_data['count'], label='valid')
+plt.legend(loc='best')
+plt.title("Train and Validation Data")
+plt.show()
+
+from fbprophet import Prophet
+
+
+
+
